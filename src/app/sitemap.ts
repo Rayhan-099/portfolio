6d10b164b5559b3b2150fed60ede244316b247d3
Rayhan <khanrayhan7038@gmail.com>
2026-05-26 21:09:08 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,38 +5,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://rayhank.vercel.app',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
-    },
-    {
-      url: 'https://rayhank.vercel.app/#projects', // Add your actual routes here
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://rayhank.vercel.app/resume.pdf',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://rayhank.vercel.app/#about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.4,
-    },
-    {
-      url: 'https://rayhank.vercel.app/#achievements',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.3,
-    },
-    {
-      url: 'https://rayhank.vercel.app/#contact',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.2,
-    }, 
+    }
   ];
 }
